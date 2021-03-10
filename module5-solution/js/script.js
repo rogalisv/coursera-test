2ccae5875ed1fbc2164bb8c62a48de7d89700b4b
Your Name <you@example.com>
2021-03-10 11:15:40 +0600

    222

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+   buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -122,7 +122,10 @@ function buildAndShowHomeHTML (categories) {
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-
+ var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+                var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,
+                    "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
+                insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
